b7e9200f0de0684f65527900670cb9c8e5a1eec9
gldeng <realgldeng@gmail.com>
2024-10-08 11:38:58 +0800

    Add Token contract

--- AElf.Script/ContextWithSystemContracts.cs
+++ AElf.Script/ContextWithSystemContracts.cs
@@ -1,3 +1,4 @@
+using AElf.Contracts.MultiToken;
 using AElf.Standards.ACS0;
 using AElf.Standards.ACS3;
 
@@ -13,4 +14,9 @@ public class ContextWithSystemContracts : Context
     public ACS0Container.ACS0Stub Genesis => this.GetInstance<ACS0Container.ACS0Stub>(
         "pykr77ft9UUKJZLVq15wCH8PinBSjVRQ12sD1Ayq92mKFsJ1i"
     );
+
+    public TokenContractContainer.TokenContractStub TokenContractStub =>
+        this.GetInstance<TokenContractContainer.TokenContractStub>(
+            "7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX"
+        );
 }
