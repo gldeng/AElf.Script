92312879d86c2ac1ac30f2eec5a1fa825151cdbc
gldeng <realgldeng@gmail.com>
2024-10-11 11:08:15 +0800

    Move config related code into ScriptWithConfig

--- AElf.Script/Script.cs
+++ AElf.Script/Script.cs
@@ -1,44 +1,6 @@
-﻿using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
-
-namespace AElf.Scripts;
+﻿namespace AElf.Scripts;
 
 public abstract class Script : ContextWithSystemContracts
 {
-    public string Environment => System.Environment.GetEnvironmentVariable("AELFSCRIPT_ENVIRONMENT") ?? "Development";
-    public string ConfigFile => $"config.{Environment}.yaml";
-
-    protected string _configContent = "";
-
-    public string ConfigContent
-    {
-        get
-        {
-            if (string.IsNullOrEmpty(_configContent))
-            {
-                if (!File.Exists(ConfigFile))
-                {
-                    throw new FileNotFoundException($"Configuration file not found: {ConfigFile}");
-                }
-
-                _configContent = File.ReadAllText(ConfigFile);
-            }
-
-            return _configContent;
-        }
-    }
-
-    public T GetConfig<T>()
-    {
-        var deserializer = new DeserializerBuilder()
-            .WithNamingConvention(CamelCaseNamingConvention.Instance)
-            .WithTypeConverter(new AddressConverter())
-            .WithTypeConverter(new HashConverter())
-            .IgnoreUnmatchedProperties()
-            .Build();
-
-        return deserializer.Deserialize<T>(ConfigContent);
-    }
-
     public abstract Task RunAsync();
 }
--- AElf.Script/ScriptWithConfig.cs
+++ AElf.Script/ScriptWithConfig.cs
@@ -1,7 +1,47 @@
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
 namespace AElf.Scripts;
 
 public abstract class ScriptWithConfig<TConfig> : Script
 {
+    public string Environment => System.Environment.GetEnvironmentVariable("AELFSCRIPT_ENVIRONMENT") ?? "Development";
+
+    public string ConfigFile => $"config.{Environment}.yaml";
+
+    private string _configContent = "";
+
+    public string ConfigContent
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(_configContent))
+            {
+                if (!File.Exists(ConfigFile))
+                {
+                    throw new FileNotFoundException($"Configuration file not found: {ConfigFile}");
+                }
+
+                _configContent = File.ReadAllText(ConfigFile);
+            }
+
+            return _configContent;
+        }
+        protected set => _configContent = value;
+    }
+
+    public T GetConfig<T>()
+    {
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .WithTypeConverter(new AddressConverter())
+            .WithTypeConverter(new HashConverter())
+            .IgnoreUnmatchedProperties()
+            .Build();
+
+        return deserializer.Deserialize<T>(ConfigContent);
+    }
+
     private TConfig _config = default(TConfig);
 
     public TConfig Config
